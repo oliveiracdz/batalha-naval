9b950d371b87994e55c8fe19244b2e9d35e47cff
Daniel Oliveira <oliveiracdz@hotmail.com>
2020-09-25 18:43:21 -0300

    Saas

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,27 +1,13 @@
 <template>
-    <Canvas color="red" ref="canvas" />
+    <Canvas ref="canvas" />
 </template>
 
 <script lang="ts">
     import Canvas from "./components/Canvas.vue";
-    import service from "./services/test";
-    import { ref, onMounted } from "vue";
 
     export default {
-        props: {
-            test: Number,
-        },
-        setup() {
-            return {};
-        },
         components: {
             Canvas,
         },
     };
 </script>
-<style>
-    html {
-        background-color: grey;
-        color: white;
-    }
-</style>
